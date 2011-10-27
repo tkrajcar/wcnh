14bafef4654b6f98dc383a26dd4edccfa2fb3653
Tim Krajcar <allegro@conmolto.org>
2011-10-27 12:09:35 -0700

    add test use of an external API to pull Github issue list into the MUSH.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source :rubygems
+
+gem "github-v3-api"
---
+++ remote/github/Github.rb
@@ -0,0 +1,22 @@
+require 'pennmush-json'
+require 'github-v3-api'
+
+module Github
+  PennJSON::register_object(self)
+
+  @api = GitHubV3API.new('52364226a675c6090b6551c609fc052d6e74c7ba')
+  @user = "tkrajcar"
+  @repo = "wcnh_softcode"
+
+  def self.pj_issues_list(*args)
+    @api.issues.list({:user => @user, :repo => @repo}).collect{|x| x.to_mush}.join('|')
+  end
+
+end
+
+class GitHubV3API::Issue
+  def to_mush
+    [self.number, self.title, self.body, self.created_at, self.updated_at].join('`')
+  end
+end
+
