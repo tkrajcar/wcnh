4690d84845aec84a3f23ceef57afdae9e54e42cb
Tim Krajcar <tim.krajcar@thenewgroup.com>
2011-10-31 15:01:13 -0700

    updated Vagrant and puppet files for proper Penn dependencies

--- wcnh_mush.pp
+++ wcnh_mush.pp
@@ -4,21 +4,14 @@ group { "puppet":
 file { '/etc/motd':
   content => "WCNH MUSH Dev Box. To be used FOR GREAT JUSTICE.\n"
 }
-package { "git-core": 
-  ensure => installed 
-}
-package { "vim":
-  ensure => installed
-}
-package { "libpcre3-dev":
-  ensure => installed
-}
-package { "gdb":
-  ensure => installed
-}
-package { "libmysqlclient-dev":
-  ensure => installed
-}
-package { "gperf":
-  ensure => installed
+
+package { "pennmush": # dependencies for pennmush, JSON server, etc.
+  ensure => installed,
+  name => [
+    'libyajl-dev',
+    'git-core',
+    'libpcre3-dev',
+    'gdb',
+    'gperf'
+  ]
 }
