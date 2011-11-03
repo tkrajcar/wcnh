bb1bb3e04492d4471fcccc8fa12b73f491babebf
Tim Krajcar <tim.krajcar@thenewgroup.com>
2011-11-03 15:50:53 -0700

    PFile system WIP.

---
+++ lib/pfile/lib-pfile.rb
@@ -0,0 +1,111 @@
+require 'wcnh'
+
+module PlayerFile
+  R = PennJSON::Remote
+
+#  def self.register_email(dbref, email)
+#    p = Player.create(:email => email)
+#
+#    Player.all_of(:email => email).count.to_s
+#  end
+
+#  def self.find_email(email)
+#    Player.all_of(:email => email).first.inspect
+
+#  end
+
+  # Add the <dbref> to the pfile for <email>. Returns _id of pfile.
+  def self.register(email,dbref)
+    return "#-1 INVALID DBREF" unless R.type(dbref) == "PLAYER"
+    p = Pfile.find_or_create_by(:email => email)
+    p.add_note("#{dbref} #{R.fullname(dbref)}",R["enactor"],"Char.new")
+    R.attrib_set("#{dbref}/PFILE",p._id.to_s)
+    #syslog("Pfile","Registered character #{dbref} #{R.fullname(dbref)} to #{p._id.to_s}.")
+    return p._id.to_s
+  end
+
+  # Renders the pfile for <arg>. <arg> could be a name, dbref, or email.
+  def self.view_file(arg)
+    return ">".red + " No file found for '#{arg}'." unless p = Pfile.locate(arg)
+    ret = titlebar("Playerfile for #{p.email} (#{p._id.to_s})") + "\n"
+    p.notes.each do |note|
+      ret << R.fullname(note.author).bold.cyan + "-".cyan + note.timestamp.strftime("%m/%d/%Y %H:%m").cyan + " (" + note.category.bold + "): ".cyan + note.text + "\n"
+    end
+    ret << footerbar
+    return ret
+  end
+
+  # Return the 30 most recent connections for <arg>. <arg> as above.
+  def self.ip(arg)
+
+  end
+
+  # Search pfile note text for the string.
+  def self.search(term) # Search text
+
+  end
+
+  # Add a new note to a pfile (search by arg)
+  def self.add(arg,note)
+
+  end
+
+  # Log a connection to a pfile.
+  def self.connect(pfile,ip,host,descriptor)
+
+  end
+
+  # Log a disconnection to a pfile.
+  def self.disconnect(pfile,descriptor)
+
+  end
+
+  class Pfile
+    include Mongoid::Document
+    field :email, :type => String
+    index :email, :unique => true
+
+    embeds_many :notes, :class_name => "PlayerFile::Note"
+    embeds_many :connections, :class_name => "PlayerFile::Connection"
+
+    def add_note(note, author, category = "Misc")
+      p "Adding note #{note} to: #{self._id}"
+      self.notes.create(:author => author, :text => note, :category => category)
+    end
+
+    # Given a dbref, player name/alias, or email, look for a pfile.
+    def self.locate(term)
+      db = R.pmatch(term)
+      if(db =~ /^#\d/)
+        # dbref match. Pull their pfile oid.
+        begin
+          Pfile.find(R.xget(db,"PFILE"))
+        rescue Mongoid::Errors::DocumentNotFound, Mongoid::Errors::InvalidFind
+          false
+        end
+      else
+        # No dbref match. Try email.
+        Pfile.first(:conditions => {:email => term})
+      end
+    end
+  end
+
+  class Note
+    include Mongoid::Document
+    embedded_in :pfiles, :class_name => "PlayerFile::Pfile"
+    field :author, :type => String
+    field :timestamp, :type => DateTime, :default => lambda { DateTime.now }
+    field :text, :type => String
+    field :category, :type => String, :default => "Misc"
+  end
+
+  class Connection
+    include Mongoid::Document
+    embedded_in :pfiles, :class_name => "PlayerFile::Pfile"
+    field :connected, :type => DateTime, :default => lambda {DateTime.now}
+    field :disconnected, :type => DateTime, :default => lambda {DateTime.now}
+    field :ip, :type => String
+    field :host, :type => String
+    field :descriptor, :type => String
+  end
+end
--- remote/PlayerFile.rb
+++ remote/PlayerFile.rb
@@ -3,11 +3,34 @@ require 'wcnh'
 module PlayerFile
   PennJSON::register_object(self)
 
-  def self.pj_register_email(dbref, email)
-    self.register_email(dbref,email)
+  def self.pj_register(email,dbref)
+    self.register(email,dbref)
   end
-  def self.pj_find_email(email)
-    self.find_email(email)
+
+  def self.pj_view(file)
+    self.view_file(file)
+  end
+
+
+
+  def self.pj_ip(file)
+
+  end
+
+  def self.pj_search(term)
+
+  end
+
+  def self.pj_add(file,category,note)
+
+  end
+
+  def self.pj_connect()
+
+  end
+
+  def self.pj_disconnect()
+
   end
 end
 
