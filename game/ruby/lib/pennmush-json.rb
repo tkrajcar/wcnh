505ae77b35b8de3e3f0467622f1ef89272b498ab
kymoon <kymoon>
2011-11-04 03:28:08 +0000

    Fixed "penn_" remote prefix to actually be a prefix. Also made minor microbenchmark-driven optimizations.

--- game/ruby/lib/pennmush-json.rb
+++ game/ruby/lib/pennmush-json.rb
@@ -49,7 +49,9 @@ module PennJSON
     # Proxy unknown invocations to remote PennMUSH. Throws RemoteError in the
     # event the remote call responds with an error.
     def self.method_missing(name, *args)
-      return SERVER.remote_invoke(@context, name.to_s.gsub("penn_",""), *args)
+      name = name.to_s
+      name = name[5..-1] if name.start_with?('penn_')
+      return SERVER.remote_invoke(@context, name, *args)
     end
 
     # Set the context hash. If we had to deal with threads, this would need to
