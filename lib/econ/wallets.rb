9e7cf2782e9a9715321831937b8bec1feb91a600
Tim Krajcar <allegro@conmolto.org>
2012-01-07 15:56:19 -0800

    put down: initial code. R.create() commented out because creating objects throws a memory hard crash to Penn at the moment.

--- lib/econ/wallets.rb
+++ lib/econ/wallets.rb
@@ -15,6 +15,7 @@ module Econ
     victim_wallet = Wallet.find_or_create_by(id: victim)
     return ">".bold.green + " You can only pay people in the same room as you!" unless R.loc(R["enactor"]) == R.loc(victim) || R.orflags(R["enactor"],"Wr").to_bool
     amount = BigDecimal.new(amount.delete(',')).round(1,:floor)
+    return ">".bold.green + " You can't pay somebody negative money!" unless amount > 0
     return ">".bold.green + " You don't have enough credits to do that!" unless sender_wallet.balance >= amount || R.orflags(R["enactor"],"Wr").to_bool
     sender_name = R.penn_name(R["enactor"])
     victim_name = R.penn_name(victim)
@@ -28,6 +29,17 @@ module Econ
   end
 
   def self.putdown(amount)
-    "Not implemented"
+    w = Wallet.find_or_create_by(id: R["enactor"])
+    amount = BigDecimal.new(amount.delete(',')).round(1,:floor)
+    return ">".bold.green + " You don't have enough credits to do that!" unless w.balance >= amount || R.orflags(R["enactor"],"Wr").to_bool
+    return ">".bold.green + " You need to put down at least 1 credit." unless amount >= 1
+    location = R.loc(R["enactor"])
+    Logs.log_syslog("ECONPUTDOWN","#{R.penn_name(R["enactor"])} put down #{amount} credits in #{location}.")
+    #putdown = R.create("#{amount} credits")
+    #R.parent(putdown,"#22")
+    #R.attrib_set("#{putdown}/amount",amount)
+    #R.tel(putdown,loc)
+    R.oemit(R["enactor"],">".bold.green + " #{R.penn_name(R["enactor"]).bold} puts down #{amount.to_s.bold.yellow} credit#{amount > 1.0 ? "s" : ""}.")
+    ">".bold.green + " You put down #{amount.to_s.bold.yellow} credits."
   end
 end
