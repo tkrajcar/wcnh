026bc6c3e92959b4bdfe26bec68e30cbe8db8aa4
Tim Krajcar <allegro@conmolto.org>
2012-02-24 17:16:14 -0800

    fix minor formatting bug if no jobs available

--- lib/econ/cargojobs.rb
+++ lib/econ/cargojobs.rb
@@ -7,7 +7,7 @@ module Econ
     streetwise_skill = R.u("#112/fn.get.skill",R["enactor"],"streetwise").to_i
     available_jobs = CargoJob.where(claimed: false).where(:expires.gte => DateTime.now).where(:visibility.lte => [economics_skill, streetwise_skill].max).asc(:expires)
     ret << "##### #{'Routing'.ljust(18)} Size   Value  Expires Manifest".cyan + "\n"
-    ret << " There don't seem to be any jobs available to you. " if available_jobs.count == 0
+    ret << " There don't seem to be any jobs available to you.\n" if available_jobs.count == 0
     available_jobs.each do |job|
       ret << job.to_mush
       ret << "\n"
@@ -37,6 +37,4 @@ module Econ
     job.save
     Logs.log_syslog("CARGOJOBCLAIM", "#{R.penn_name(R["enactor"])} claimed job #{job.number} (#{job._id}).")
   end
-
-
 end
