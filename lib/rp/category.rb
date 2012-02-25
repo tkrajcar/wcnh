8cf86204962eae89cccd836e969c072f8e328dfc
Brandon Rice <brice84@gmail.com>
2012-02-25 17:13:52 -0500

    Typo.

--- lib/rp/category.rb
+++ lib/rp/category.rb
@@ -4,7 +4,7 @@ module RP
   class Category
     include Mongoid::Document
     
-    field :title, type: String
+    field :name, type: String
     
     has_many :items, :class_name => "RP::Item"
   end
