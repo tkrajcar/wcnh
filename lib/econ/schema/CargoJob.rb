c13343c0acffe3607701bf9151cde3bc9de4d5aa
Tim Krajcar <allegro@conmolto.org>
2012-02-26 07:50:51 -0800

    slight pricing tweak

--- lib/econ/schema/CargoJob.rb
+++ lib/econ/schema/CargoJob.rb
@@ -1,7 +1,7 @@
 module Econ
   class CargoJob
-    BASE_CARGO_RATE_MIN = 40.0
-    BASE_CARGO_RATE_MAX = 60.0
+    BASE_CARGO_RATE_MIN = 30.0
+    BASE_CARGO_RATE_MAX = 45.0
     TIME_FACTOR_MULTIPLIER = [0,0.8,1.0,1.5,2.0,3.0]
     GRADE_MULTIPLIER = [0,0.6,0.8,1.0,1.2,1.4,2.0]
     GRADE_WORDS = ['', 'surplus', 'low-grade', 'unremarkable', 'fine', 'exquisite']
