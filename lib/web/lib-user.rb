eef31c795bee8865d4e0a1fe8bb7c7df14a05c53
Tim Krajcar <allegro@conmolto.org>
2012-03-03 20:09:03 -0800

    clarify user messaging for web/register

--- lib/web/lib-user.rb
+++ lib/web/lib-user.rb
@@ -7,6 +7,6 @@ module Web
     return ">".bold + " You don't seem to have a registered email address. Contact an admin to get this resolved." unless !email.nil? && !email.empty?
     result = WebAPIClient.register(email,password,R["enactor"],R.penn_name(R["enactor"]))
     return ">".bold + " There was a problem: #{result["message"]}" unless result["success"]
-    return ">".bold + " Registration completed!"
+    return ">".bold + " Registration completed! You can now login to the website using your registered email, #{email.bold}, and password #{password.bold}."
   end
 end
