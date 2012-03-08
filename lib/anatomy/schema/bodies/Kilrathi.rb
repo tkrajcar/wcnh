0b53a103608b10d923e5581144174e4cbfbd33a0
Brandon <brice84@gmail.com>
2012-03-07 19:40:31 -0500

    Fix Kilrathi schema.

--- lib/anatomy/schema/bodies/Kilrathi.rb
+++ lib/anatomy/schema/bodies/Kilrathi.rb
@@ -7,11 +7,11 @@ module Anatomy
     @parts = {
       "Head" => [Anatomy::Head, {:mass => 9.0}],
       "Torso" => [Anatomy::Torso, {:mass => 24.0}],
-      "LArm" => [Anatomy::Arm, {:mass => 9.5}],
-      "RArm" => [Anatomy::Arm, {:mass => 9.5}],
+      "Left Arm" => [Anatomy::Arm, {:mass => 9.5}],
+      "Right Arm" => [Anatomy::Arm, {:mass => 9.5}],
       "Groin" => [Anatomy::Groin, {:mass => 5.5}],
-      "LLeg" => [Anatomy::Leg, {:mass => 14.0}],
-      "RLeg" => [Anatomy::Leg, {:mass => 14.0}],
+      "Left Leg" => [Anatomy::Leg, {:mass => 14.0}],
+      "Right Leg" => [Anatomy::Leg, {:mass => 14.0}],
       "Tail" => [Anatomy::Tail, {:mass => 2.0}]
       }
   end
