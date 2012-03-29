908445353525c99591016c409936bd02994bed75
Brandon <brice84@gmail.com>
2012-03-29 17:03:05 -0400

    Sort event listing on +cal by event date.

--- lib/calendar/calendar.rb
+++ lib/calendar/calendar.rb
@@ -6,7 +6,7 @@ module Calendar
     tz = R.penn_default(R["enactor"] + "/tz", "+0") 
     start = (month.length > 0 && year.length > 0) ? self.date_to_datetime(DateTime.now.midnight, "01 #{month} #{year}", "+0") : DateTime.now
     range = start..start + 30.days
-    events = Event.where(date: range)
+    events = Event.where(date: range).desc(:date)
 
     return "> ".bold.red + "No events found." unless (events.count > 0)
     ret = titlebar("Calendar Events from #{start.strftime("%b %d %Y")} to #{range.last.strftime("%b %d %Y")}") + "\n"
