2c7359c397e0841730490b12d862292f1e445809
Brandon <brice84@gmail.com>
2012-03-29 18:48:16 -0400

    Kill unnecessary fields.  Add reference for Category<->Sub and User<->Sub.  Set default permissions.

--- lib/bboard/schema.rb
+++ lib/bboard/schema.rb
@@ -9,7 +9,7 @@ module BBoard
     # Reserve the user class instead of just pulling the dbref on the fly in case we want a place to store
     # global bboard user settings.
     
-    embeds_many :subscriptions, :class_name => "BBoard::Subscription"
+    has_many :subscriptions, :class_name => "BBoard::Subscription"
   end
   
   class Category
@@ -18,20 +18,34 @@ module BBoard
     field :name, type: String
     field :permission_type, type: String 
     field :permission_value, type: String
+    field :anonymous, type: Boolean, :default => false
     
     has_many :posts, :class_name => "BBoard::Post"
+    has_many :subscriptions, :class_name => "BBoard::Subscription"
+    
+    def can_read?(dbref)
+      return true if self.permission_type.nil?
+      return true if R.orflags(dbref, "Wr").to_bool
+      return true if self.permission_type == "announce"
+      return false
+    end
+    
+    def can_write?(dbref)
+      return true if self.permission_type.nil?
+      return true if R.orflags(dbref, "Wr").to_bool
+      return false
+    end
   end
   
   class Post
     include Mongoid::Document
     include Mongoid::Timestamps
     
-    field :number, type: Integer, :default => lambda {Counters.next("BBPost")}
-    index :number, :unique => true
     field :sticky, type: Boolean, :default => false
     field :author, type: String
     field :title, type: String
     field :body, type: String
+    field :parent_id , type: String # Posts that are threads belong to a parent post.
     
     belongs_to :category, :class_name => "BBoard::Category"
   end
@@ -39,10 +53,10 @@ module BBoard
   class Subscription
     include Mongoid::Document
     
-    field :category, type: String
     field :read_posts, type: Array
     
-    embedded_in :user, :class_name => "BBoard::User"
+    belongs_to :user, :class_name => "BBoard::User"
+    belongs_to :category, :class_name => "BBoard::Category"
   end
   
 end
