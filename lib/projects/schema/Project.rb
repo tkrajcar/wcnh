1dabc573bceda1c778a61ec3a314e4e8a839300f
Brandon Rice <brice84@gmail.com>
2012-04-22 23:13:12 -0400

    Additional fields.  Validations.  Started the work method.

--- lib/projects/schema/Project.rb
+++ lib/projects/schema/Project.rb
@@ -7,12 +7,16 @@ module Projects
     
     field :title, type: String
     field :details, type: String
-    field :managers, type: Array
-    field :members, type: Array
+    field :managers, type: Array, :default => []
+    field :members, type: Array, :default => []
     field :public, type: Boolean, :default => false
+    field :completed, type: Boolean, :default => false
+    field :interval, type: Integer, :default => 3 # Number of hours between eligible work periods
     
     embeds_many :tasks, :class_name => "Projects::Task"
     embeds_many :efforts, :class_name => "Projects::Effort"
+    
+    validates_presence_of :title, message: "The project must have a title."
     
     def inspect
       ret = titlebar("Project Board: #{self.title}") + "\n"
@@ -22,6 +26,19 @@ module Projects
       end
       
       ret << footerbar
+    end
+    
+    def work(dbref, task_name)
+      return "> ".bold.red + "You aren't a member of that project." unless (self.managers + self.members).include?(dbref)
+      return "> ".bold.red + "There's no such task associated with this project." unless task = self.tasks.where(:name => Regexp.new("(?i)#{task_name}"))
+      
+      effort = self.efforts.where(:character => dbref).desc(:created_at).first
+      unless (effort.nil? || DateTime.now > effort.created_at + self.interval) then
+        return "> ".bold.red + "You must wait until #{effort.created_at + self.interval} before doing additional work on this project."
+      end
+      
+      p "Do work."
+      return
     end
   end
   
--- lib/projects/schema/Task.rb
+++ lib/projects/schema/Task.rb
@@ -8,11 +8,17 @@ module Projects
     
     field :name, type: String
     field :skill, type: String
-    field :difficulty, type: Integer
-    field :current, type: Integer
-    field :goal, type: Integer  
+    field :difficulty, type: Integer, :default => 1
+    field :current, type: Integer, :default => 0
+    field :goal, type: Integer, :default => 0
     
     belongs_to :project, :class_name => "Projects::Project"
+    
+    validates_presence_of :name, message: "The task must have a name."
+    validates_presence_of :skill, message: "The task must have a skill requirement."
+    
+    validates_numericality_of :difficulty, greater_than: 0, message: "Difficulty must be an integer >0."
+    validates_numericality_of :goal, greater_than: 0, message: "Goal must be an integer >0."  
   end
   
 end
