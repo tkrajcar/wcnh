b0d9782fc2ce05393cb9708f2dac36f0cb78458b
Brandon Rice <brice84@gmail.com>
2012-05-12 16:35:30 -0400

    Method for converting decimal to fraction odds.

--- lib/Float.rb
+++ lib/Float.rb
@@ -9,4 +9,23 @@ class Float
     time << "#{seconds.to_i} seconds" if seconds.to_i > 0
     return time.join(', ')
   end
+  
+  def to_frac_odds
+    base = (self - 1).round(1)
+    div = 1
+    
+    until base % 1 == 0 do
+      div += 1 
+      base = (base * div).round(1)
+    end
+    
+    result = (self * div).to_i
+    
+    if result % div == 0
+      result = result / div
+      div = div / div
+    end
+    
+    return "#{result}:#{div}"
+  end
 end
