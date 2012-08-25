ac8e69a45f1b43e4d74680389aa68d6f74f52024
Brandon Rice <brice84@gmail.com>
2012-08-25 00:21:04 -0400

    Finish the model portion of the firearms reload method.

--- lib/items/Firearms.rb
+++ lib/items/Firearms.rb
@@ -6,7 +6,19 @@ module Items
       return [false, :invalid_ammo] unless ammo.kind.class.name.split('::').last == "Ammunition"
       return [false, :incompatible_ammo] unless self.attribs['ammunition'] == ammo.attribs['name']
       return [false, :insufficient_ammo] unless ammo.attribs['amount'] > 0
-      return true
+      
+      if (ammo.attribs['amount'] < (amount = self.attribs['max_rounds'] - self.attribs['rounds']))
+        self.attribs['rounds'] += ammo.attribs['amount']
+        Items.remove(Items::MUSH_FUNCTIONS, ammo)
+      else
+        self.attribs['rounds'] = self.attribs['max_rounds']
+        ammo.attribs['amount'] -= amount
+        ammo.save
+      end
+      
+      self.save
+      
+      return [true, amount]
     end
   end
 end
