77e23c510ebd96db00dd65c562c87e3bc94bbb9b
Brandon Rice <brice84@gmail.com>
2012-08-26 15:53:16 -0400

    Show rounds and max_rounds as visible attribs on guns.

--- lib/items/schema/Gun.rb
+++ lib/items/schema/Gun.rb
@@ -5,7 +5,7 @@ module Items
     field :rounds, type: Integer, default: 0
     field :ammunition, type: String 
 
-    @showable = {mass: 'kg', damage: '', skill: '', ammunition: ''}
+    @showable = {mass: 'kg', damage: '', skill: '', ammunition: '', rounds: '', max_rounds: ''}
     @is_gun = true
   end
 
